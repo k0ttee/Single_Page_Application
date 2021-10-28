38d69693aeeb23dcee303dabf7a53149650d1ddc
k0ttee <k0ttee@yandex.ru>
2021-10-28 15:49:06 +0300

    Update 0000_php.sh

--- PHP/0000_php.sh
+++ PHP/0000_php.sh
@@ -1,3 +1,5 @@
 #сессии в оперативке
 sudo echo 'tmpfs /ramsessions tmpfs noatime,nodiratime,nodev,nosuid,size=64M 0 0' >> /etc/fstab
 sudo echo 'session.save_path = "/ramsessions"' >> /etc/php/7.4/fpm/php.ini
+
+#перезагрузка Windows (если дело в WSL)
