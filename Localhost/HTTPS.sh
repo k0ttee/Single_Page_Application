278ac46d1cf5db495c86379dec572f29816d5c8f
k0ttee <k0ttee@yandex.ru>
2021-10-28 11:36:07 +0300

    Update HTTPS.sh

--- Localhost/HTTPS.sh
+++ Localhost/HTTPS.sh
@@ -2,22 +2,45 @@
 # Power Shell #
 ###############
 
-Win -> PowerShell -> запуск от имени администратора
+#от имени администратора
+Win -> PowerShell
 
+#накатываю менеджер пакетов chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+#накатываю утилиту
 choco install mkcert
+
+#устанавливаю локальный центр сертификации
 mkcert -install
-mkcert -cert-file C:\Users\Азъ\fullchain.pem -key-file C:\Users\Азъ\privkey.pem localhost
+
+#генерирую сертификаты
+
+mkcert -cert-file C:\Users\Азъ\localhost-fullchain.pem -key-file C:\Users\Азъ\localhost-privkey.pem localhost
+mkcert -cert-file C:\Users\Азъ\127.0.0.1-fullchain.pem -key-file C:\Users\Азъ\127.0.0.1-privkey.pem 127.0.0.1
+mkcert -cert-file C:\Users\Азъ\192.168.1.1-fullchain.pem -key-file C:\Users\Азъ\192.168.1.1-privkey.pem 192.168.1.1
 
 #######
 # WSL #
 #######
 
 #готовлю директории (пути как в продакшене)
+
 sudo mkdir -p /etc/letsencrypt/live/localhost/
+sudo mkdir -p /etc/letsencrypt/live/127.0.0.1/
+sudo mkdir -p /etc/letsencrypt/live/192.168.1.1/
+
 sudo chmod 777 /etc/letsencrypt/live/localhost/
+sudo chmod 777 /etc/letsencrypt/live/127.0.0.1/
+sudo chmod 777 /etc/letsencrypt/live/192.168.1.1/
 
-sudo mv /mnt/c/Users/Азъ/fullchain.pem /etc/letsencrypt/live/localhost/fullchain.pem
-sudo mv /mnt/c/Users/Азъ/privkey.pem /etc/letsencrypt/live/localhost/privkey.pem
+#переношу сертификаты
 
+sudo mv /mnt/c/Users/Азъ/localhost-fullchain.pem /etc/letsencrypt/live/localhost/fullchain.pem
+sudo mv /mnt/c/Users/Азъ/localhost-privkey /etc/letsencrypt/live/localhost/privkey.pem
+
+sudo mv /mnt/c/Users/Азъ/127.0.0.1-fullchain.pem /etc/letsencrypt/live/127.0.0.1/fullchain.pem
+sudo mv /mnt/c/Users/Азъ/127.0.0.1-privkey /etc/letsencrypt/live/127.0.0.1/privkey.pem
+
+sudo mv /mnt/c/Users/Азъ/192.168.1.1-fullchain.pem /etc/letsencrypt/live/192.168.1.1/fullchain.pem
+sudo mv /mnt/c/Users/Азъ/192.168.1.1-privkey /etc/letsencrypt/live/192.168.1.1/privkey.pem
