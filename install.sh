07dd6fb16b5888f055e3c32b26c0a0daf9f1ef6e
k0ttee <k0ttee@yandex.ru>
2021-11-06 09:45:05 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo -i
+
+apt update
+
+apt install htop curl wget jq
+
+apt install nginx
+
+apt install php-fpm
+apt install php7.4-mbstring
+apt install php7.4-pgsql
+apt install php7.4-gd
+
+apt install postgresql
+apt install pgbouncer
+
+apt clean
