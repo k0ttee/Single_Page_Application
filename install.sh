af34c5160fcacdb6b3e32150c5ae017a6d39d83d
k0ttee <k0ttee@yandex.ru>
2021-11-06 09:49:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,34 @@
+#рут
 sudo -i
 
+#обновления
 apt update
+apt upgrade
 
+#утилиты
 apt install htop curl wget jq
 
+#веб
 apt install nginx
 
+#пхп
 apt install php-fpm
 apt install php7.4-mbstring
 apt install php7.4-pgsql
 apt install php7.4-gd
 
+#база
 apt install postgresql
 apt install pgbouncer
 
+#почта
+#маил-утил и опен-дким и ещё что-то?
+
+#биткоин
+#поискать репозиторий
+
+#лайткоин
+#есть в репозиториях
+
+#чистка
 apt clean
