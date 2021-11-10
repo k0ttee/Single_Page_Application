2ac61e9fc52487203d558e01394ca06d601f76a2
k0ttee <k0ttee@yandex.ru>
2021-11-11 01:44:21 +0300

    Update 0000_install.sh

--- Finance/0000_install.sh
+++ Finance/0000_install.sh
@@ -47,6 +47,9 @@ alias grep="grep --color=auto"
 alias ls="ls -Fa --color"
 alias ll="ls -l --color"
 alias disk="df -H / && echo && df -i /"
+alias start-btc-main="bitcoind -daemon -chain=main"
+alias start-btc-test="bitcoind -daemon -chain=test"
+alias stop-btc="bitcoin-cli -rpcuser=user -rpcpassword=password stop"
 
 #подхватить конфиг баш
 source /root/.bashrc
